313d832fda1a48b7c52e09848180d684ad6cd505
manlikexavy <chibokaxavier@gmail.com>
2024-04-04 13:36:50 +0100

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>
+        ChatGpt clone
+      </title>
       <body className={inter.className}>{children}</body>
     </html>
   );
