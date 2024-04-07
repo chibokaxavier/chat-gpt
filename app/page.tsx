fcfa3865905438f756198a0f645f24ead49b49df
manlikexavy <chibokaxavier@gmail.com>
2024-04-07 22:18:24 +0100

    edited the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="bg-yellow-200">
-        hello 
+      <h1 className="">
+        
          </h1>
     </main>
   );
